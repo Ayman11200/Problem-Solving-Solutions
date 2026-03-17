afa77fa059973ad77aaffb2a32996db8fae11b74
Ayman11200 <aymanaldesoky112003@gmail.com>
2026-03-17 20:48:15 +0200

    Problem I Welcome_for_you_with_Conditions

---
+++ Assuit ICPE/Sheet 01/I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short A,B;
+    cin >> A >> B ;
+
+    if(A >= B)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+}
